34fba0285d23002f02d2a8d7679df68f599c4dbe
Red-CS <red.devcs@gmail.com>
2021-06-19 18:13:31 -0400

    add colors, update website

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import ThemeColors from "../public/colors.js";
 
 export default function Home() {
   return (
@@ -20,13 +21,28 @@ export default function Home() {
         <div className={styles.editor}>
           <Image
             src="/img/example.png"
-            width={960}
-            height={490}
+            width={1919}
+            height={979}
             quality={100}
           />
         </div>
-        <h2>Color Palette</h2>
-        <div className={styles.palette}></div>
+        <h3 className={styles.h3}>Color Palette</h3>
+        <div className={styles.palette}>
+          {ThemeColors.palette.map((color, index) => {
+            return (
+              <div className={styles["color-box"]} key={index + 1}>
+                <div className={styles["color-label"]} key={(index + 1) * -1}>
+                  {color}
+                </div>
+                <button
+                  className={styles["color-button"]}
+                  style={{ backgroundColor: color }}
+                  key={index}
+                ></button>
+              </div>
+            );
+          })}
+        </div>
       </main>
     </div>
   );
